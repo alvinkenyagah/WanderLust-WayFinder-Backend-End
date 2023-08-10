91c05e5c8ce590c4d9b34976b30e31a67f102682
alvinkenyagah <alvin47k@gmail.com>
2023-08-10 12:23:04 +0300

    Update user validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,8 +5,8 @@ class User < ApplicationRecord
   has_many :itineraries
   has_many :destinations, through: :itineraries
 
-  validates :username, presence: true, uniqueness: true
-  validates :email, presence: true, uniqueness: true
+  validates :username, presence: true, uniqueness: true , on: :create
+  validates :email, presence: true, uniqueness: true , on: :create
   validates :first_name, presence: true
   validates :last_name, presence: true
   validates :password, presence: true, on: :create 
