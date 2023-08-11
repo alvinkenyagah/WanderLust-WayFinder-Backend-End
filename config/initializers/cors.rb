6e47419bf49b2d2217c5b691bc9ea373735667a2
alvinkenyagah <alvin47k@gmail.com>
2023-08-11 12:13:26 +0300

    Configure cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
     allow do
-      origins '*' # Update this with the domains allowed to access your API
+      origins 'https://mellifluous-cajeta-21a88c.netlify.app/' # Update this with the domains allowed to access your API
       resource '*', headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head]
     end
   end
